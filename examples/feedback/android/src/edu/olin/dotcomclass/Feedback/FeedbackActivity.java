c51ac6b4e331251977028dded0a90d8cb6d08f37
Mark L. Chang <mark.chang@gmail.com>
2011-02-07 22:21:07 -0500

    added listview

--- examples/feedback/android/src/edu/olin/dotcomclass/Feedback/FeedbackActivity.java
+++ examples/feedback/android/src/edu/olin/dotcomclass/Feedback/FeedbackActivity.java
@@ -1,10 +1,13 @@
 package edu.olin.dotcomclass.Feedback;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class FeedbackActivity extends Activity
+public class FeedbackActivity extends ListActivity
 {
+    public static final String TAG = "FEEDBACK";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
