cd16405a2ea3065fbef0ca170efd944bbcd05863
Mark L. Chang <mark.chang@gmail.com>
2011-02-17 00:21:13 -0500

    scaffolded android application

---
+++ examples/android-maps/src/edu/olin/dotcom/androidmaps/AndroidMaps.java
@@ -0,0 +1,15 @@
+package edu.olin.dotcom.androidmaps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidMaps extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
