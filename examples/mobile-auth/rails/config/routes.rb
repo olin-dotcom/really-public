a2c943bedd7941974e0c2f325d66e5ec8b0ec981
Mark L. Chang <mark.chang@gmail.com>
2011-03-18 12:54:34 -0400

    added nifty-generators, scaffolded Posts

--- examples/mobile-auth/rails/config/routes.rb
+++ examples/mobile-auth/rails/config/routes.rb
@@ -1,4 +1,10 @@
 MobileAuth::Application.routes.draw do
+  resources :posts
+
+  devise_for :users
+  
+  root :to => "posts#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
