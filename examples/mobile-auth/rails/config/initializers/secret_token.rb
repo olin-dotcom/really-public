6a17e3345678a0fb2031be86e3a9b6c7df73cd42
Mark L. Chang <mark.chang@gmail.com>
2011-03-18 12:49:41 -0400

    rails new

---
+++ examples/mobile-auth/rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileAuth::Application.config.secret_token = '088ef2a14aba59fb21e87a5073d65e7af54ea7fd5cb31df17e71b3baa43722c3fc0ec575f28036d9fa65361b6a60af1bc768fab2fa0d793908294981949cf64a'
