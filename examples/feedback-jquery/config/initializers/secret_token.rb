2da8b5f0ba1435ef5f254be5ff786e375fb9bbb5
Mark L. Chang <mark.chang@gmail.com>
2011-04-04 10:56:10 -0400

    added a jquery example

---
+++ examples/feedback-jquery/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedbackJquery::Application.config.secret_token = '489d0589763ea7fb9d4832cec4c776e0030a0628e306fd8819696e0b3b1f3ced79c68ac5d9c0a9f3c18731e679c360534909f84acb30d6fc7b8fdd739716616a'
