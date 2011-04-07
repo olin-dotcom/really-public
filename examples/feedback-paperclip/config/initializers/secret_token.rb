2e168d27e23ab1285a9c3950444ffe5e9f39fcf3
Mark L. Chang <mark.chang@gmail.com>
2011-04-07 19:14:31 -0400

    rails new, rspec, nifty-generator/layout, haml, jquery

---
+++ examples/feedback-paperclip/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedbackPaperclip::Application.config.secret_token = '64595eac37d9aef406d4a7f07811050e450e48fbf95ed86f298cd14f49d0b5b7c5ca2b05a98ac3e4c6caf164f9b26b57fa61ab95342f579e854decdc3eee62c9'
