1c7c9c0a5456b64e5beb338e4826940aa27d55ed
Mark L. Chang <mark.chang@gmail.com>
2011-04-07 22:00:49 -0400

    using s3 now

--- examples/feedback-paperclip/app/models/comment.rb
+++ examples/feedback-paperclip/app/models/comment.rb
@@ -1,11 +1,14 @@
 class Comment < ActiveRecord::Base
   attr_accessible :name, :caption, :photo
   
-  has_attached_file :photo,
+  has_attached_file :photo, :storage => :s3,
     :styles => {
       :thumb => "100x100#",
       :small  => "150x150>",
       :medium => "200x200>",
       :large => "500x500>"
-      }
+      },
+    :s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml",
+    :bucket => "chipsahoy-dev",
+    :path => ":class/:id/:basename_:style.:extension"
 end
