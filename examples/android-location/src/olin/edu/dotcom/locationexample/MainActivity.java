6fba12b063b6867fecd4b5f1f82d9bd8745e1316
Mark L. Chang <mark.chang@gmail.com>
2011-04-26 09:29:02 -0400

    initial location example

---
+++ examples/android-location/src/olin/edu/dotcom/locationexample/MainActivity.java
@@ -0,0 +1,55 @@
+package olin.edu.dotcom.locationexample;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity
+{
+    private static final String TAG = "LOCATION_EXAMPLE";
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        setupLocationManager();
+    }
+
+    private void makeUseOfNewLocation(Location location)
+    {
+        Log.v(TAG, "Making use of new location");
+
+        Log.v(TAG, "Location: " + location.toString());
+    }
+
+    private void setupLocationManager()
+    {
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+              // Called when a new location is found by the network location provider.
+              makeUseOfNewLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+          };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+    }
+}
